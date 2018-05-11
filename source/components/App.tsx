a15ae5d915a76f30c6019a92cae78261f10652eb
Fernando Medina <fmedina@bondacom.com>
2018-05-11 17:46:43 -0300

    fix: Removed unused packages

--- source/components/App.tsx
+++ source/components/App.tsx
@@ -4,7 +4,7 @@ class App extends React.Component<{}, {}> {
     public render(): JSX.Element {
         return (
             <div>
-                OLIS
+                OLISS
             </div>
         );
     }
